54e58d2019a1ecafc04ce89638d3df11a77ce100
Heiko Holz <heiko@whysoseriousgames.de>
2019-07-16 14:39:34 +0200

    Update app_service.dart

--- frontend/WebAnnotation/lib/app_service.dart
+++ frontend/WebAnnotation/lib/app_service.dart
@@ -6,7 +6,7 @@ import 'dart:html';
 @Injectable()
 class AppService {
   //static final SERVER_URL = "http://ec2-18-221-249-140.us-east-2.compute.amazonaws.com:8000";
-  static final SERVER_URL = "http://http://134.2.128.120:8000/";
+  static final SERVER_URL = "http://134.2.128.120:8000/";
 
   String infoMessageText = "";
   String errorMessageText = "";
